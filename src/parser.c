be4065e21863c541349705d0cb83bfa4af430851
ygt-ernsy <yigiterensoy@hotmail.com>
2026-06-07 21:40:06 +0300

    feat: implemented the parser.c which will be used for parsing user input

---
+++ src/parser.c
@@ -0,0 +1,66 @@
+#include "parser.h" // NOTE: this or ../include/parser.h ?
+#include <ctype.h>
+#include <err.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* parses a given line and puts the individual words into a given char* array */
+int parse(char *lineptr, char *tokens[])
+{
+    char word[BUFSIZ];
+    int i = 0;
+    int start = 0;
+    int temp;
+    char c;
+
+    // TODO: is BUFSIZ correct way?
+    while (getword(word, lineptr, BUFSIZ) >= 0)
+    {
+        char *token = malloc((BUFSIZ + 1) * sizeof(char));
+
+        if (token == NULL)
+            err(EXIT_FAILURE, "malloc"); // should this return -1 like getword
+
+        strlcpy(token, word, BUFSIZ);
+
+        tokens[i++] = token; // TODO: Add error handling
+        start = temp;
+    }
+
+    // TODO: Check for a way to know the tokens have ended
+
+    return 1; // TODO: Is this what it should return?
+}
+
+static int getword(char *word, char *lineptr, int lim)
+{
+    char *c = lineptr;
+    int i = 0;
+
+    if (lineptr == NULL)
+        return -1;
+
+    while (isspace(*c))
+    {
+        i++;
+        c++;
+    }
+
+    if (*c == '\0' || *c == '\n')
+        return -1;
+
+    for (; isalnum(*c) && lim-- > 0; c++, i++)
+    {
+        *word++ = *c; // NOTE: Is this safe?
+
+        // this is so getword skips the already read parts in the next iteration
+        // TODO: Find a better way to handle this
+        *(lineptr + i) = ' ';
+    }
+
+    *word++ = '\0';
+
+    return word[0];
+}
